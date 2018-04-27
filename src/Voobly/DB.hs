fee3f3e8f2c59354bccd58fa7d76c689e235a88b
bowswung <bowswung@gmail.com>
2018-04-27 20:34:36 +0100

    REquest player data

--- src/Voobly/DB.hs
+++ src/Voobly/DB.hs
@@ -6,29 +6,112 @@ module Voobly.DB where
 
 
 import RIO
-
+import RIO.Time
+import Voobly.TH
 import Data.Acid
 import Data.SafeCopy
 import Network.HTTP.Client
 import Control.Monad.State
 import qualified Control.Lens as L
+import qualified Data.IxSet.Typed as IxSet
 
 
-query' :: MonadIO m => QueryEvent event => AcidState (EventState event) -> event -> m (EventResult event)
-query' a b = liftIO $ query a b
+newtype PlayerId = PlayerId {playerIdToText :: Text} deriving (Eq, Ord, Show)
+newtype Team = Team Int deriving (Eq, Ord, Show)
+newtype CivilisationId = CivilisationId Int deriving (Eq, Ord, Show)
+newtype MatchId = MatchId Int deriving (Eq, Ord, Show)
 
-update' :: MonadIO m => UpdateEvent event => AcidState (EventState event) -> event -> m (EventResult event)
-update' a b = liftIO $ update a b
+
+
+data Player = Player {
+  playerId :: PlayerId,
+  playerName :: Text,
+  playerMatchIds :: Vector MatchId,
+  playerLastCompletedUpdate :: Maybe UTCTime
+} deriving (Eq, Ord, Show)
+
+data Ladder =
+    LadderRm
+  | LadderRmTeam
+  deriving (Eq, Ord, Show)
+
+ladderId :: Ladder -> Int
+ladderId LadderRm = 131
+ladderId LadderRmTeam = 132
+
+data PlayerLadder = PlayerLadder {
+  playerLadderPlayerId :: PlayerId,
+  playerLadderLadder :: Ladder,
+  playerLadderRating :: Int,
+  playerLadderWins  :: Int,
+  playerLadderLoss :: Int
+} deriving (Eq, Ord, Show)
+
+
+data Match = Match {
+  matchId :: MatchId,
+  matchDate :: UTCTime,
+  matchDuration :: DiffTime,
+  matchLadder :: Ladder,
+  matchMap :: Text,
+  matchPlayers :: [MatchPlayer],
+  matchWinner :: Team
+} deriving (Eq, Ord, Show)
+
+data Civilisation = Civilisation {
+  civilisationId :: CivilisationId,
+  civilisationName :: Text
+} deriving (Eq, Ord, Show)
+
+data MatchPlayer = MatchPlayer {
+  matchPlayerPlayerId :: PlayerId,
+  matchPlayerCiv :: CivilisationId,
+  matchPlayerPreRating :: Int,
+  matchPlayerPostRating :: Int,
+  matchPlayerTeam :: Team
+} deriving (Eq, Ord, Show)
+
+data PlayerLadderProgress = PlayerLadderProgress {
+  playerLadderProgressLadder :: Ladder
+, playerLadderProgressLastPageHandled :: Maybe Int
+, playerLadderProgressLastCompleted :: Maybe UTCTime
+} deriving (Eq, Ord, Show)
+
+
+defaultPlayerLadderProgress :: Ladder -> PlayerLadderProgress
+defaultPlayerLadderProgress l = PlayerLadderProgress l Nothing Nothing
+
+playerLadderUniqIdx :: PlayerLadder -> (PlayerId, Ladder)
+playerLadderUniqIdx a = (playerLadderPlayerId a, playerLadderLadder a)
+
+makeSimpleIxSet "PlayerSet" ''Player ['playerId, 'playerLastCompletedUpdate]
+makeSimpleIxSet "PlayerLadderSet" ''PlayerLadder ['playerLadderPlayerId, 'playerLadderLadder, 'playerLadderUniqIdx]
+makeSimpleIxSet "CivilisationSet" ''Civilisation ['civilisationId]
+makeSimpleIxSet "MatchSet" ''Match ['matchId, 'matchLadder]
+makeSimpleIxSet "PlayerLadderProgressSet" ''PlayerLadderProgress ['playerLadderProgressLadder]
+
+
 
 data DB = DB {
   _dbCookies :: [Cookie]
+, _dbPlayers :: PlayerSet
+, _dbPlayerLadders :: PlayerLadderSet
+, _dbCivilisations :: CivilisationSet
+, _dbMatches :: MatchSet
+, _dbPlayerLadderProgress :: PlayerLadderProgressSet
 }
+
 L.makeLenses ''DB
 
 
 emptyDb :: DB
 emptyDb = DB {
   _dbCookies = []
+, _dbPlayers = IxSet.empty
+, _dbPlayerLadders = IxSet.empty
+, _dbCivilisations = IxSet.empty
+, _dbMatches = IxSet.empty
+, _dbPlayerLadderProgress = IxSet.empty
 }
 
 updateDB :: DB -> Update DB ()
@@ -44,15 +127,51 @@ updateCookies cookies = modify (L.set dbCookies cookies)
 getCookies :: Query DB [Cookie]
 getCookies = L.view dbCookies <$> ask
 
+getPlayer :: PlayerId -> Query DB (Maybe Player)
+getPlayer pid = (IxSet.getOne . IxSet.getEQ pid) <$> L.view dbPlayers <$> ask
+
+getPlayerLadder :: PlayerId -> Ladder -> Query DB (Maybe PlayerLadder)
+getPlayerLadder pid ladder = (IxSet.getOne . IxSet.getEQ pid . IxSet.getEQ ladder) <$> L.view dbPlayerLadders <$> ask
+
+updatePlayer :: Player -> Update DB ()
+updatePlayer a = modify (over dbPlayers (IxSet.updateIx (playerId a) a))
+
+updatePlayerLadder :: PlayerLadder -> Update DB ()
+updatePlayerLadder a = modify (over dbPlayerLadders (IxSet.updateIx (playerLadderUniqIdx a) a))
+
+getPlayerLadderProgress :: Ladder -> Query DB (Maybe PlayerLadderProgress)
+getPlayerLadderProgress pid = (IxSet.getOne . IxSet.getEQ pid) <$> L.view dbPlayerLadderProgress <$> ask
+
+
+updatePlayerLadderProgress :: PlayerLadderProgress -> Update DB ()
+updatePlayerLadderProgress a = modify (over dbPlayerLadderProgress (IxSet.updateIx (playerLadderProgressLadder a) a))
+
 
 $(deriveSafeCopy 0 'base ''Cookie)
 $(deriveSafeCopy 0 'base ''DB)
+$(deriveSafeCopy 0 'base ''PlayerId)
+$(deriveSafeCopy 0 'base ''Player)
+$(deriveSafeCopy 0 'base ''Ladder)
+$(deriveSafeCopy 0 'base ''PlayerLadder)
+$(deriveSafeCopy 0 'base ''Team)
+$(deriveSafeCopy 0 'base ''CivilisationId)
+$(deriveSafeCopy 0 'base ''Match)
+$(deriveSafeCopy 0 'base ''Civilisation)
+$(deriveSafeCopy 0 'base ''MatchPlayer)
+$(deriveSafeCopy 0 'base ''MatchId)
+$(deriveSafeCopy 0 'base ''PlayerLadderProgress)
 
 $(makeAcidic ''DB [
   'updateDB,
   'getDB,
   'updateCookies,
-  'getCookies
+  'getCookies,
+  'getPlayer
+  , 'updatePlayer
+  , 'getPlayerLadder
+  , 'updatePlayerLadder
+  , 'getPlayerLadderProgress
+  , 'updatePlayerLadderProgress
   ])
 
 
--- src/Voobly/Scraper.hs
+++ src/Voobly/Scraper.hs
@@ -6,8 +6,10 @@ import Voobly.DB
 import RIO
 import qualified RIO.ByteString.Lazy as BL
 import qualified RIO.Text as T
-
+import qualified RIO.Vector as V
+import RIO.Time
 import Data.Acid
+import Data.Tree
 import Data.Acid.Local
 import Options.Applicative
 import Network.HTTP.Client
@@ -15,17 +17,33 @@ import Network.HTTP.Client.TLS
 import Network.HTTP.Types
 
 import Control.Monad.State.Strict
+import qualified Text.HTML.Parser       as P
+import qualified Text.HTML.Tree       as P
+import qualified Data.Csv as Csv
 
+import qualified RIO.List.Partial as Partial
+import qualified RIO.List as L
+
+import qualified Data.IxSet.Typed as IxSet
+import qualified  Text.Regex as Regex
 
 data AppError =
-  AppErrorCouldntLogIn Text
+    AppErrorCouldntLogIn Text
+  | AppErrorInvalidHtml Text
+  | AppErrorParserError Text
   deriving (Show, Typeable)
 
 instance Exception AppError
 
+data Command =
+    CommandRun
+  | CommandQuery
+
 data Options = Options
   { username   :: Text
   , password   :: Text
+  , debug :: Bool
+  , runCommand :: Command
   }
 
 
@@ -35,12 +53,24 @@ optionsParser = Options
        long "username"
     <> short 'u'
     <> help "Voobly username"
+    <> value ""
     )
- <*> strOption (
+  <*> strOption (
        long "password"
     <> short 'p'
     <> help "Voobly password"
+    <> value ""
     )
+  <*> switch (
+       long "debug"
+    <> help "For debugging"
+    )
+   <*> subparser (
+      ( command "run"          (info (helper <*> pure CommandRun)                 (progDesc "Run the scraper" ))
+     <> command "query"  (info (helper <*> pure CommandQuery)          (progDesc "Show data"))
+      )
+    )
+
 
 optionsParserInfo :: ParserInfo Options
 optionsParserInfo = info (optionsParser <**> helper) fullDesc
@@ -88,7 +118,16 @@ runScraper = do
       let appEnv = AppEnv lf options acid
           appState = AppState manager
       runRIO appEnv $ runAppM appState $ do
-        initialise
+        case runCommand options of
+          CommandRun -> do
+            initialise
+            logInfo $ "*** Scraping ladder " <> displayShow LadderRm <> " ***"
+            scrapeLadder LadderRm
+            scrapePlayers
+          CommandQuery -> do
+            db <- query' GetDB
+            logDebug $ "Players: " <> (displayShow $ (IxSet.size $ _dbPlayers db))
+            logDebug $ displayShow $ take 20 (IxSet.toList $ _dbPlayers db)
 
 
 runDir :: FilePath
@@ -147,23 +186,284 @@ authenticate = do
     else throwM $ AppErrorCouldntLogIn "Tried with supplied username and password"
 
 
+
+ladderPageUrl :: Ladder -> Int -> Text
+ladderPageUrl l p = vooblyUrl <> "/ladder/ranking/" <> T.pack (show $ ladderId l) <> "/" <> T.pack (show p)
+
+
+scrapeLadder :: Ladder -> AppM ()
+scrapeLadder l = do
+  skip <- do
+    appEnv <- ask
+    if (debug . appEnvOptions $  appEnv)
+      then do
+        db <- query' GetDB
+        if (IxSet.size $ _dbPlayers db)  > 100
+          then pure True
+          else pure False
+      else pure False
+  if skip
+    then do
+      logDebug $ "Skipping scrape ladder for --debug because we have enough players already"
+    else do
+      startProgress <- fmap (fromMaybe $ defaultPlayerLadderProgress l) $ query' (GetPlayerLadderProgress l)
+      now <- getCurrentTime
+      let p = case playerLadderProgressLastPageHandled startProgress of
+                Just i -> i + 1
+                Nothing -> 0
+          needsUpdate =
+            case playerLadderProgressLastCompleted startProgress of
+              Nothing -> True
+              Just d -> diffUTCTime now d  > (3600 * 24 * 7) -- every week
+      if needsUpdate
+        then do
+          tags <- makeHTMLTreesRequest $ ladderPageUrl l p
+          table <- extractLadderTable tags
+          let rows = drop 1 $ extractFromTree table (isTagOpen "tr")
+          tups <- mapM extractLadderRow rows
+
+          if null tups
+            then do
+              logInfo $ "Page " <> displayShow p <> " didn't contain any data - ladder is finished!"
+              update' $ UpdatePlayerLadderProgress (startProgress{playerLadderProgressLastPageHandled = Nothing, playerLadderProgressLastCompleted = Just now})
+            else do
+              void $ mapM (updatePlayerLadders l) tups
+              logInfo $ "Page " <> displayShow p <> " processed successfully with " <> (displayShow $ length tups) <> " players (e.g." <> displayShow (Partial.head tups)
+              update' $ UpdatePlayerLadderProgress (startProgress{playerLadderProgressLastPageHandled = Just p})
+              scrapeLadder l
+        else do
+          logInfo $ "Player ladder " <> displayShow l <> " is up to date as of " <> displayShow (playerLadderProgressLastCompleted startProgress) <> " and does not need to be updated now"
+          return ()
+
+
+scrapePlayers :: AppM ()
+scrapePlayers = do
+  db <- query' GetDB
+  now <- getCurrentTime
+  let weekago = addUTCTime (-3600 * 24 * 7) now
+  let playersToUpdate = IxSet.toList $ IxSet.getLT (Just weekago) $ _dbPlayers db
+  void $ mapM scrapePlayer playersToUpdate
+
+
+playerMatchUrl :: Player -> Int -> Text
+playerMatchUrl p page = vooblyUrl <> "/profile/view/" <> (playerIdToText . playerId $ p) <> "/Matches/games/matches/user/" <> (playerIdToText . playerId $ p) <> "/0/" <> T.pack (show page)
+
+divInt :: Int -> Int -> Double
+divInt = (/) `on` fromIntegral
+
+scrapePlayer :: Player -> AppM ()
+scrapePlayer p = do
+  r <- makeTextRequest $ playerMatchUrl p 0
+  totalMatches <- extractMatchCount r
+  let matchesMissing = totalMatches - (V.length $ playerMatchIds p)
+      pagesToRequest = ceiling $ matchesMissing `divInt` 10
+  if pagesToRequest < 1
+    then do
+      logDebug $ "no new matches for player " <> displayShow p
+      return ()
+    else do
+      let pages = reverse [1 .. pagesToRequest]
+      void $ mapM (scrapePlayerPage p) pages
+
+
+scrapePlayerPage :: Player -> Int -> AppM ()
+scrapePlayerPage p page = do
+  r <- makeTextRequest $ playerMatchUrl p 0
+  matchIds <- extractPlayerMatchIds r
+  logDebug $ displayShow matchIds
+
+extractMatchCount :: Text -> AppM Int
+extractMatchCount t = do
+  let r = Regex.mkRegex "<div class=\"count\">Displaying [0-9]+ - [0-9]+ out of ([0-9]+) matches"
+  case Regex.matchRegex r (T.unpack t) of
+    Just [x] -> runParserFromText $ T.pack x
+    _ -> throwM $ AppErrorInvalidHtml "Expected regex to match exactly one numeric value in extractMatchCount"
+
+
+
+
+
+extractPlayerMatchIds :: Text -> AppM [Int]
+extractPlayerMatchIds t = do
+  let r = Regex.mkRegex "<td bgcolor=\"#[^\"]+\" style=\"\"><a href=\"https:\/\/voobly\.com\/match\/view\/17649526\">#([0-9]+)<\/a><\/td>"
+  case Regex.matchRegex r (T.unpack t) of
+    Just xs ->
+      if length xs > 0 && length xs < 11
+        then runParserFromText $ T.pack x
+        else throwM $ AppErrorInvalidHtml "Expected between 1 and 10 match ids"
+    _ -> throwM $ AppErrorInvalidHtml "Expected regex to match exactly one numeric value in extractMatchCount"
+
+
+
+
+type LadderRow = (Text, PlayerId, Int, Int, Int)
+
+
+
+updatePlayerLadders :: Ladder -> LadderRow -> AppM ()
+updatePlayerLadders l (name, pid, rating, wins, loss) = do
+  let p = Player pid name V.empty Nothing
+  update' $ UpdatePlayer p
+  let pl = PlayerLadder{
+             playerLadderPlayerId = pid
+           , playerLadderLadder = l
+           , playerLadderRating = rating
+           , playerLadderWins = wins
+           , playerLadderLoss = loss
+           }
+  update' $ UpdatePlayerLadder pl
+
+
+
+extractLadderRow :: Tree P.Token -> AppM LadderRow
+extractLadderRow (Node _ forest) =
+  case extractFromForest forest (isTagOpen "td") of
+    _c1:c2:c3:c4:c5:_c6:_c7:[] -> do
+      (name, pid) <- extractNameAndIdFromToken c2
+      rating <- extractIntFromToken c3
+      wins <- extractIntFromToken c4
+      loss <- extractIntFromToken c5
+      return (name, pid, rating, wins, loss)
+    _ -> throwM $ AppErrorInvalidHtml "Expected 7 tokens in ladder row"
+
+
+extractIntFromToken :: Tree P.Token -> AppM Int
+extractIntFromToken tok =
+  case flattenForest $ extractFromTree tok (isContextText) of
+    [P.ContentText t] -> runParserFromText t
+    _ -> throwM $ AppErrorInvalidHtml "Expected one contenttext token for extracting an Int"
+
+extractNameAndIdFromToken :: Tree P.Token -> AppM (Text, PlayerId)
+extractNameAndIdFromToken t =
+  case flatten $ Partial.last $ extractFromTree t (isTagOpen "a") of
+    (P.TagOpen _ attrs):(P.ContentText name):[] -> do
+
+      case findAttributeValue "href" attrs of
+        Nothing -> throwM $ AppErrorInvalidHtml "Expected href attribute in extractNameAndIdFromToken"
+        Just href -> do
+          let tId = Partial.last $ T.split (== '/') href
+          return (name, PlayerId . T.strip $ tId)
+
+
+
+    _ -> throwM $ AppErrorInvalidHtml "Expected open tag, contenttext for extractNameAndIdFromToken"
+
+
+
+runParserFromText :: (Csv.FromField a) => Text -> AppM a
+runParserFromText t =
+  case Csv.runParser . Csv.parseField $ encodeUtf8 t of
+    Left err -> throwM $ AppErrorParserError $ T.pack err
+    Right a -> return a
+
+findAttributeValue :: Text -> [P.Attr] -> Maybe Text
+findAttributeValue x ls = do
+  (P.Attr _ v) <- L.find (\(P.Attr n _) -> n == x) ls
+  pure v
+
+
+
+isTagOpen :: Text -> P.Token -> Bool
+isTagOpen x (P.TagOpen t _) = x == t
+isTagOpen _ _ = False
+
+isContextText :: P.Token -> Bool
+isContextText (P.ContentText _) = True
+isContextText _ = False
+
+flattenForest :: Forest a -> [a]
+flattenForest = concat . map flatten
+
+extractFromForest :: Forest a -> (a -> Bool) -> Forest a
+extractFromForest f x = concat $ map (\t -> extractFromTree t x) f
+
+extractFromTree :: Tree a -> (a -> Bool) -> Forest a
+extractFromTree tree@(Node a forest) f =
+  let l = if f a then [tree] else []
+  in l ++ (concat $ map (\t -> extractFromTree t f) forest)
+
+makeHTMLTreesRequest :: Text -> AppM ([P.Token])
+makeHTMLTreesRequest u = do
+  baseReq <- parseRequest $ T.unpack $ u
+  res <- makeRequest baseReq
+  return $ P.canonicalizeTokens $ P.parseTokens $ decodeUtf8With lenientDecode (BL.toStrict $ responseBody res)
+
+makeTextRequest :: Text -> AppM Text
+makeTextRequest u = do
+  baseReq <- parseRequest $ T.unpack $ u
+  res <- makeRequest baseReq
+  return $ decodeUtf8With lenientDecode (BL.toStrict $ responseBody res)
+
+extractLadderTable :: [P.Token] -> AppM (Tree P.Token)
+extractLadderTable ts = do
+  let (_,_,_,res) =
+        (flip execState) (False, False, False, []) $ mapM collectToken ts
+  case P.tokensToForest res of
+    Left e -> throwM $ AppErrorInvalidHtml $ utf8BuilderToText . displayShow $ e
+    Right [f] -> return f
+    Right _ -> throwM $ AppErrorInvalidHtml "Expected exactly one table tree"
+
+  where
+    collectToken :: P.Token -> State (Bool, Bool, Bool, [P.Token]) ()
+    collectToken tok = do
+      (afterLadder, inTable, lastTagWasImg, res) <- get
+      case tok of
+        P.ContentText t -> do
+          if (afterLadder && inTable)
+            then put (afterLadder, inTable, False, res ++ [tok])
+            else if t == "Ladder Players"
+              then put (True, inTable, False, res)
+              else return ()
+        P.TagOpen t _ -> do
+          if (afterLadder && inTable)
+            then put (afterLadder, inTable, t == "img", res ++ [tok])
+            else if (afterLadder && t == "table")
+              then put (afterLadder, True, False, res ++ [tok])
+              else return ()
+        P.TagClose t  -> do
+          if inTable && lastTagWasImg && t == "a"
+            then put (afterLadder, inTable, False, res)
+            else
+              if (afterLadder && t == "table")
+                then put (False, False, False, res ++ [tok])
+                else if afterLadder && inTable
+                  then put (afterLadder, inTable, False, res ++ [tok])
+                  else return ()
+        _ -> if afterLadder && inTable
+              then put (afterLadder, inTable, False, res ++ [tok])
+              else return ()
+
+
+
+
 defaultRequestHeaders :: [Header]
 defaultRequestHeaders = [
     ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"),
-    ("Referer", "https://www.voobly.com/login"),
     ("Origin", "https://www.voobly.com")
   ]
 
 makeRequest :: Request -> AppM (Response BL.ByteString)
 makeRequest req = do
-  env <- ask
   st <- get
-  origCookieJar <- fmap createCookieJar $ query' (appEnvAcid env) GetCookies
+  origCookieJar <- fmap createCookieJar $ query' GetCookies
   let req' = req {
         cookieJar = Just origCookieJar,
         requestHeaders = requestHeaders req ++ defaultRequestHeaders
       }
   res <- liftIO $ httpLbs req' (appStateManager st)
-  update' (appEnvAcid env) $ UpdateCookies (destroyCookieJar $ responseCookieJar res)
+  update' $ UpdateCookies (destroyCookieJar $ responseCookieJar res)
   return res
+
+
+
+query' :: (QueryEvent event, EventState event ~ DB) => event -> AppM (EventResult event)
+query' e = do
+  env <- ask
+  liftIO $ query (appEnvAcid env) e
+
+update' :: (UpdateEvent event, EventState event ~ DB) => event -> AppM (EventResult event)
+update' e = do
+  env <- ask
+  liftIO $ update (appEnvAcid env) e
+
 
